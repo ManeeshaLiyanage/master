d8d9641f72545858f12fa2fddac0b0056401e467
ManeeshaLiyanage <shalindiliyanage@gmail.com>
2019-03-16 02:36:31 -0700

    by maneesha

--- app/src/main/java/com/example/coolspot/MainActivity.java
+++ app/src/main/java/com/example/coolspot/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.coolspot;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -80,9 +82,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-//        if (id == R.id.nav_camera) {
-//            // Handle the camera action
-//        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.place_order) {
+            Intent i = new Intent(getApplicationContext(),PlaceOrderActivity.class);
+            startActivity(i);
+       }
+// else if (id == R.id.nav_gallery) {
 //
 //        } else if (id == R.id.nav_slideshow) {
 //
@@ -98,4 +102,7 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
+
 }
